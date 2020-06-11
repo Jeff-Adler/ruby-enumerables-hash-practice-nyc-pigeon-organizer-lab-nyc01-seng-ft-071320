038eac7a73f2f9cfee11b72af299f1872fb29432
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 18:23:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
 end
+
+#for each element in the top-level, if there is another layer of nesting, call that layer of nesting.
+#Otherwise, turn that string into a category
