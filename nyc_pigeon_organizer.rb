caf800e0b4b39e140caad7e24fabd9d75a75ffad
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 18:29:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-
+  data.each do |key,value|
+    category 
+  end
 end
 
 #for each element in the top-level, if there is another layer of nesting, call that layer of nesting.
