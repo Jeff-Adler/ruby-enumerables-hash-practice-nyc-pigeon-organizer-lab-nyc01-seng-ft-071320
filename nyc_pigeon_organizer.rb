f7387df7f1424d9c91ade6ce5a5e1de9469edbfb
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 18:44:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+
+  #this block of code returns an organized_hash of all the pigeon names as keys
+  #pointing to empty hashes
   organized_hash = {}
   data.each do |category,values|
     values.each do |value,names|
@@ -11,7 +14,7 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  binding.pry 
+
 end
 
 #for each element in the top-level, if there is another layer of nesting, call that layer of nesting.
