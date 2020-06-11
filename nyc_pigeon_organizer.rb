b9078280ef2675e20485e77e6aa0273c069ae04d
Jeff Adler <jeffm.adler@gmail.com>
2020-06-11 19:18:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
       data.each do |category,values|
         values.each do |value,names|
           names.each do |name|
-            if name == pigeon && attribute.to_s == value.to_s
+            if name == pigeon
                 organized_hash[pigeon][attribute] << value.to_s
                 binding.pry
             end
